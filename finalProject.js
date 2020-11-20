cd91972d7fd20475d375e3fe851135fe69cc70d6
Aimee Rivard <aimeerivard@Aimees-MBP.attlocal.net>
2020-11-19 16:26:58 -0800

    final final copy with css and js updates

--- finalProject.js
+++ finalProject.js
@@ -1,23 +1,34 @@
 var list = [
   {
     restName: "Veggie Grill",
-    restAddress: "123 veggie st",
+    restAddress: "3055 Olin Ave, San Jose, CA 95128",
     restWebsite: "www.veggiegrill.com",
-    restDescription: "Lorem ipsum dolor sit amet",
+    restDescription:
+      "Vegan chain offering sandwiches, salads, faux meats & sides in a modern, counter-service setting.",
     restLocation: "southBay",
     restCuisine: "fastFood",
   },
   {
     restName: "Pizza Flora",
-    restAddress: "123 pizza st",
+    restAddress: "78 S 1st St, San Jose, CA 95113",
     restWebsite: "www.pizzaflora.com",
-    restDescription: "Ipsum dolor sit amet, consectetur",
+    restDescription: "Vegan and vegetarian pizzeria in downtown San Jose.",
     restLocation: "southBay",
     restCuisine: "pizza",
+  },
+  {
+    restName: "The Butcher's Sons",
+    restAddress: "1954 University Ave, Berkeley, CA 94704",
+    restWebsite: "www.thebutchersveganson.com",
+    restDescription:
+      "Charming, industrial-chic bakery & vegan deli for pastries, coffee & creative vegetable-based mains.",
+    restLocation: "eastBay",
+    restCuisine: "healthy",
   },
 ];
 
 function addCard() {
+  // take the string inside the input field and add it to the restCard
   list.push({
     restName: document.getElementById("name").value,
     restAddress: document.getElementById("address").value,
@@ -26,60 +37,23 @@ function addCard() {
     restLocation: document.getElementById("location").value,
     restCuisine: document.getElementById("cuisine").value,
   });
+
+  // clear input and select after submit button is clicked
   document.getElementById("name").value = "";
   document.getElementById("address").value = "";
   document.getElementById("website").value = "";
   document.getElementById("description").value = "";
   document.getElementById("location").value = "location";
   document.getElementById("cuisine").value = "cuisine";
-  // // create a var for restCard
-  // var restCard = document.createElement('div')
-
-  // //update innerHTML with the child elements
-  // restCard.innerHTML = `
-  //         <section class="column">
-  //             <div>${list[i].restName}</div>
-  //             <div>${list[i].restAddress}</div>
-  //             <div>${list[i].restWebsite}</div>
-  //         </section>
-  //         <aside>
-  //            ${list[i].restDescription}
-  //         </aside>
-  // `;
-  // document.getElementById('list').appendChild(restCard)
-  //     }
   renderRestCard();
 }
 
 var renderRestCard = function () {
+  // clear the list before rendering the next restCard so the list does not populate each time
   var clearList = document.getElementById("list");
   clearList.textContent = "";
 
-  for (var i = 0; i < list.length; i++) {
-    // create a var for restCard
-    var restCard = document.createElement("div");
-
-    //update innerHTML with the child elements
-    restCard.innerHTML = `
-            <div class="arrayCard">   
-                <section class="column" id="arraySection">
-                    <div id="restCardTitle">${list[i].restName}</div>
-                    <div>${list[i].restAddress}</div>
-                    <div><a href="http://${list[i].restWebsite}">${list[i].restWebsite}</a></div>
-                </section>
-                <aside>
-                ${list[i].restDescription}
-                </aside>
-            </div>
-        `;
-    document.getElementById("list").appendChild(restCard);
-  }
-};
-
-var filterButton = function () {
-  var clearList = document.getElementById("list");
-  clearList.textContent = "";
-
+  // for loop through each restaurant in list to render each card if it matches the filter
   for (var i = 0; i < list.length; i++) {
     var rest = list[i];
     var filterLoc = document.getElementById("filterLocation").value;
@@ -99,8 +73,8 @@ var filterButton = function () {
                     <div class="arrayCard">   
                         <section class="column" id="arraySection">
                             <div id="restCardTitle">${list[i].restName}</div>
-                            <div>${list[i].restAddress}</div>
-                            <div><a href="http://${list[i].restWebsite}">${list[i].restWebsite}</a></div>
+                            <div class="restCardDesc">${list[i].restAddress}</div>
+                            <div class="restCardDesc"><a href="http://${list[i].restWebsite}">${list[i].restWebsite}</a></div>
                         </section>
                         <aside>
                         ${list[i].restDescription}
@@ -110,10 +84,4 @@ var filterButton = function () {
       document.getElementById("list").appendChild(restCard);
     }
   }
-
-  // grab list
-  // for loop
-  // if statement
-  // inside if statement it has to be based off of what is selected from the options
-  // if it evaluates to true, do the render card function
 };
